801954492900a5bbba5cd5f835a9b806ca22321a
Maftunakhon <129736276+QayumovaMaftunakhon@users.noreply.github.com>
2023-05-21 23:15:35 -0700

    Add files via upload

---
+++ JS/myscript.js
@@ -0,0 +1,10 @@
+/*const header = document.querySelector('.side-header');
+const p = document.querySelector('.p');
+document.querySelector('.design on').onchange = function() {
+    if(this.checked){
+header.classList.add
+    }
+    else{
+
+    }
+}*/
